814fccff7077a2a4831863ba537172655fe8ca7d
Janesh Sharma <52086988+Janesh98@users.noreply.github.com>
2019-09-25 16:37:31 +0100

    change all http to https to avoid conflicts

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,7 +9,7 @@ def main():
     movies.append(Movie("Inception", "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
     "https://www.youtube.com/watch?v=d3A3-zSOBT4"))
 
-    movies.append(Movie("Batman: The Dark Knight Rises", "http://www.movienewsletters.net/photos/117274R1.jpg",
+    movies.append(Movie("Batman: The Dark Knight Rises", "https://www.movienewsletters.net/photos/117274R1.jpg",
     "https://www.youtube.com/watch?v=g8evyE9TuYk"))
     ft.open_movies_page(movies)
 
