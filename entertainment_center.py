94eae08a8ab3cb03f7db66f0d8f3bc69293a8c98
Janesh Sharma <52086988+Janesh98@users.noreply.github.com>
2019-09-30 00:14:16 +0100

    implemented an imdb api extracting movie id code. Captured img and youtube trailer urls

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,16 +1,14 @@
 from media import Movie
 import fresh_tomatoes as ft
+from mov_dict import d
 
 def main():
     movies = []
-    movies.append(Movie("Interstellar", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-    "https://www.youtube.com/watch?v=zSWdZVtXT7E"))
+    for title, urls in d.items():
+        poster = urls[0]
+        trailer = urls[1]
+        movies.append(Movie(title, poster, trailer))
 
-    movies.append(Movie("Inception", "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
-    "https://www.youtube.com/watch?v=d3A3-zSOBT4"))
-
-    movies.append(Movie("Batman: The Dark Knight Rises", "https://www.movienewsletters.net/photos/117274R1.jpg",
-    "https://www.youtube.com/watch?v=g8evyE9TuYk"))
     ft.open_movies_page(movies)
 
 if __name__ == '__main__':
